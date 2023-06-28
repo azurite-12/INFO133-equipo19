54174abae341a148ff27c7aff2e039be1c087aa0
MeatOfJustice <33604227+MeatOfJustice@users.noreply.github.com>
2023-06-28 10:50:37 -0400

    excel a csv

--- Parte B/Programas/getSecurityData.py
+++ Parte B/Programas/getSecurityData.py
@@ -7,8 +7,9 @@ archivo=basepath+"/pazciudadanacl_exportacion_14062023115901.xlsx"
 
 df_com=pd.read_excel(archivo,sheet_name="Fundación Paz Ciudadana")
 df_com.head()
+df_com.to_csv(basepath+'\TablaSeguridad.csv', index = None, header=True)
 
 #regiones=df_com.groupby(["Región"]).sum()
-comunal=df_com[["Comuna","Tasa cada 100 Mil"]]
-comunal.plot(kind="bar")
-plt.show()
+#comunal=df_com[["Comuna","Tasa cada 100 Mil"]]
+#comunal.plot(kind="bar")
+#plt.show()
