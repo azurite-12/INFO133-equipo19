d77c43e10373a34ee9c81bc620ce69edbddba249
azurite-12 <diegoseba19@gmail.com>
2023-06-28 11:24:11 -0400

    seguridad

--- Parte B/Programas/getSecurityData.py
+++ Parte B/Programas/getSecurityData.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-basepath="C:/Users/emper/Documents/BD"
-archivo=basepath+"/pazciudadanacl_exportacion_14062023115901.xlsx"
+directorioActual = os.path.dirname(os.path.abspath(__file__))
+rel_pat1 = '../Datos/pazciudadanacl_exportacion_14062023115901.xlsx'
+archivo = os.path.join(directorioActual, rel_pat1)
+#basepath='../Datos'
+#archivo=basepath+'/pazciudadanacl_exportacion_14062023115901.xlsx'
 
 df_com=pd.read_excel(archivo,sheet_name="Fundación Paz Ciudadana")
 df_com.head()
+df_com.to_csv('./TablaSeguridad.csv', index = None, header = True)
 
 #regiones=df_com.groupby(["Región"]).sum()
 comunal=df_com[["Comuna","Tasa cada 100 Mil"]]
